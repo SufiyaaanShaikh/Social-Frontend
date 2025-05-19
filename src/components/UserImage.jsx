c42c38c97c4714a6f8c7730566436858c221ee5d
SufiyaaanShaikh <sufisk2526@gmail.com>
2025-05-19 18:48:06 +0530

    Backend Api integrated

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -11,7 +11,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`https://social-backend-u3jw.vercel.app/assets/${image}`}
       />
     </Box>
   );
